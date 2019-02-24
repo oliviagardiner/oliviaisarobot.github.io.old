792d7eb4dc7e609bc5417f02504894f994209dd4
Olivia Gardiner <oliv.gardiner@gmail.com>
2019-02-24 08:54:15 +0100

    Structure overhaul

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,39 +1,21 @@
 $(document).ready(function() {
 
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 300) {
-      $('#scroll-top').fadeIn('slow');
+    if ($(this).scrollTop() > 150) {
+      $('#to-the-top').fadeIn('slow');
     } else {
-      $('#scroll-top').fadeOut('slow');
+      $('#to-the-top').fadeOut('slow');
     }
   });
 
-  $('#menu-icon').click(function() {
-    $('nav').animate({width: ["toggle", "swing"]}, 300);
-  });
-
-  $('#scroll-top').click(function() {
+  $('#to-the-top').click(function() {
     $('html').animate({scrollTop:0}, 500, 'swing');
   });
 
-  $('.print-me').click(function() {
-    window.print();
-  });
-
-  $('.showcase-content').click(function() {
-    src = $(this).attr('src');
-    $('.lightbox').css('background-image', 'url(' + src + ')');
-    $('.lightbox').fadeIn();
-  });
-
-  $('.lightbox-close').click(function() {
-    $('.lightbox').fadeOut();
-  });
-
-  $(window).keydown(function(event) {
-    if ( event.which == 27 ) {
-      $('.lightbox').fadeOut();
-    }
+  $('#toggle-social-menu-mobile').click(function() {
+    if ($('.social-menu').hasClass('open')) {
+      $('.social-menu').removeClass('open');
+    } else $('.social-menu').addClass('open');
   });
 
 });
